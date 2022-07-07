4141df903e7d9812637dbc51c9eb74f07f859410
sswastik02 <sswastik02@gmail.com>
2022-07-07 17:54:32 +0530

    Implemented Pipeline for Credit Card Model
    [+/-] backend/ML/credit_card.py : Implemented Class for Credit Card Fraud Detection
      [+] backend/ML/main.py        : Created a generalised driver program for Models

--- backend/ML/credit_card.py
+++ backend/ML/credit_card.py
@@ -3,99 +3,114 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
+from sklearn.pipeline import Pipeline
 
 from keras.models import Sequential
 from keras.layers import Dense
+from scikeras.wrappers import KerasClassifier
 
-import pickle
+import joblib
 import sys, os
 
-MODEL_SAV_FILE = 'credit-card.sav'
+class CreditCard:
 
-def create_model(csv_file):
-    df = pd.read_csv(csv_file)
-    dataset = df.values
-    print(df.shape)
-    
-    X = dataset[:,1:10] # Catergorical Data
-    Y = dataset[:,10]  # Class Data
+    PIPE_SAV_FILE = 'credit-card.sav'
 
-    min_max_scaler = preprocessing.MinMaxScaler()
-    X_scale = min_max_scaler.fit_transform(X)
+    def get_model(self):
+        # model architecture
+        model = Sequential([
+            Dense(32, activation='relu', input_shape=(9,)),
+            Dense(32, activation='relu'),
+            Dense(1, activation='sigmoid'),
+        ])
 
-    # training data
-    X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X_scale, Y, test_size=0.3)
+        # compile model
+        model.compile(
+            optimizer='sgd',
+            loss='binary_crossentropy',
+            metrics=['accuracy']
+        )
 
-    # testing data
-    X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
+        return model
 
+    def get_dataset(self,csv_file):
+        df = pd.read_csv(csv_file)
+        dataset = df.values
+        print(df.shape)
+        
+        X = dataset[:,1:10] # Catergorical Data
+        Y = dataset[:,10]  # Class Data
 
-    # model architecture
-    model = Sequential([
-        Dense(32, activation='relu', input_shape=(9,)),
-        Dense(32, activation='relu'),
-        Dense(1, activation='sigmoid'),
-    ])
+        return X,Y
 
-    # compile model
-    model.compile(
-        optimizer='sgd',
-        loss='binary_crossentropy',
-        metrics=['accuracy']
-    )
-
-    # fit model
-    hist = model.fit(
-        X_train,
-        Y_train,
-        batch_size=32, epochs=100,
-        validation_data=(X_val, Y_val)
-    )
-
-    # evaluate model
-    print(model.evaluate(X_test, Y_test)[1])
-
-    # save model
-    pickle.dump(model,open(os.path.join(sys.path[0],MODEL_SAV_FILE),'wb'))
+    def get_scaler(self,dataset=None):
+        
+        X,_ = dataset
+        min_max_scaler = preprocessing.MinMaxScaler()
+        min_max_scaler = min_max_scaler.fit(X)
+        return min_max_scaler
 
 
-def predict_fraud():
-    loaded_model = pickle.load(open(os.path.join(sys.path[0],MODEL_SAV_FILE),'rb'))
-    prediction = loaded_model.predict([[
-        0.2034085 , 
-        0.0911139 , 
-        0. , 
-        0. ,
-        0. ,
-        0. , 
-        0. , 
-        0. , 
-        0. ,
-    ]])
 
-    return prediction
+    def process_dataset(self,dataset,scaler):
 
-def get_csv():
-    if len(sys.argv) > 1 and sys.argv[1] != "":
-        if(os.path.exists(sys.argv[1])):
-            return sys.argv[1]
-        print("",sys.argv[1], "not found")
-        sys.exit(0)
-    print("","Usage : python3 file.py path/to/csv")
-    sys.exit(0)
+        X, Y = dataset
 
-def main():
-    if not os.path.exists(os.path.join(sys.path[0],MODEL_SAV_FILE)):
-        print(MODEL_SAV_FILE,"not found")
-        CSV_FILE = get_csv()
-        create_model(CSV_FILE)
-    else :
-        op = input('\n\n credit card model already created\n Would you like to recreate ? (y/n): ')
-        if op == 'y':
-            CSV_FILE = get_csv()
-            create_model(CSV_FILE)
-    print("Prediction:",predict_fraud())
+        X_train, X_val_and_test, Y_train, Y_val_and_test = train_test_split(X, Y, test_size=0.3)
+        X_val, X_test, Y_val, Y_test = train_test_split(X_val_and_test, Y_val_and_test, test_size=0.5)
 
-if __name__ == "__main__":
-    main()
+        X_val = scaler.transform(X_val)
+        # this needs to be transformed seperately because they are passed directly to the model for validation
+        
+        return X_train, X_test, X_val, Y_train, Y_test, Y_val
+
+    def create_and_train_model(self,csv_file):
+        dataset = self.get_dataset(csv_file)
+        scaler = self.get_scaler(dataset)
+        processed_dataset = self.process_dataset(dataset,scaler)
+        X_train, X_test, X_val, Y_train, Y_test, Y_val = processed_dataset
+
+        # keras classifier wrapper class for pipeline use
+        clf = KerasClassifier(
+            self.get_model(),
+            batch_size=32, 
+            epochs=100,
+        )
+
+        # pipeline definition
+        pipe = Pipeline([
+            ('scaler', preprocessing.FunctionTransformer(scaler.transform)),
+            ('clf',clf),
+        ])
+
+        
+        # fit model
+        pipe.fit(
+            X_train,
+            Y_train,
+            clf__validation_data=(X_val, Y_val) # (model_name_in_pipeline)__validation_data
+        )
+
+        # evaluate model
+        print(pipe.score(X_test, Y_test))
+
+        # save pipe
+        joblib.dump(pipe,os.path.join(sys.path[0],self.PIPE_SAV_FILE))
+
+
+    def evaluate(self,csv_file):
+        loaded_pipe = joblib.load(os.path.join(sys.path[0],self.PIPE_SAV_FILE))
+        dataset = self.get_dataset(csv_file)
+        scaler = self.get_scaler(dataset)
+        processed_dataset = self.process_dataset(dataset,scaler)
+        X_train, X_test, X_val, Y_train, Y_test, Y_val = processed_dataset
+
+        return loaded_pipe.score(X_test,Y_test)
+
+
+    def predict(self,data):
+        loaded_pipe = joblib.load(os.path.join(sys.path[0],self.PIPE_SAV_FILE))
+        prediction = loaded_pipe.predict([data])
+
+        return prediction
     
