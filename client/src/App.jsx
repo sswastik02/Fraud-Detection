b4bdf4ffaa6545d510d0b4ed6da3530118ccf1ea
Architlall <archit10dgp@gmail.com>
2022-07-27 17:12:19 +0530

    web3 connection between react and solidity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,14 +5,14 @@ import Demo from "./components/Demo";
 import Footer from "./components/Footer";
 import "./App.css";
 import NavbarNew from "./components/frontend/Navbar";
-
+import ContractBtns from "./components/addTransaction"
 function App() {
   return (
     <EthProvider>
       <div id="App" >
         
           <NavbarNew/>
-          
+          <ContractBtns/>
         
       </div>
     </EthProvider>
