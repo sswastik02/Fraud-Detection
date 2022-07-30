7eed8e79ac92fc6c8dabbace3f9dbbab962886d6
docilefiasco <epshita.hs@gmail.com>
2022-07-30 17:45:12 +0530

    controlled form connected with backend

--- client/src/screens/CheckoutScreen.js
+++ client/src/screens/CheckoutScreen.js
@@ -1,16 +1,12 @@
-import React, { Component} from 'react';
+import React, { Component } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link, useHistory } from "react-router-dom";
 import "./CheckoutScreen.css";
 import Navbar from "../components/Navbar";
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
-
-
-
-export default function Checkout(){
-
+export default function Checkout() {
   const uid = localStorage.getItem("USER_ID");
   console.log(uid);
   const [firstname, setFirstName] = useState("");
@@ -23,143 +19,162 @@ export default function Checkout(){
   const [cardholdername, setCardholderName] = useState("");
   const [card, setCard] = useState("");
 
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const body = JSON.stringify({
-      firstname: firstname,
-      lastname: lastname,
-      address: address,
-      zip: zip,
-      city: city,
-      country: country,
-      cardholdername: cardholdername,
-      card: card
-    })
-  
-  await axios
-  .post(`http://localhost:5000/api/userinfo/:${uid}`, {
-   data:body  
-  })
-  .then((res) => {
-    console.log(res);
-    window.location = "/receiver";
+
    
-  })
 
-  .catch((err) => console.log(err));
-};
+    await axios
+      .put(`http://localhost:5000/api/userinfo/${uid}`, {
+        firstname: firstname,
+        lastname: lastname,
+        address: address,
+        zip: zip,
+        city: city,
+        country: country,
+        cardholdername: cardholdername,
+        card: card,
+      })
+      .then((res) => {
+        console.log(res);
+        
+      })
 
- 
-return (
-  <div>
-    <div className="container">
-      <div className="form">
-        <form>
-          <div className="title">
-            <h2>Shipping &amp; Billing</h2>
-           
-          </div>
-          <div className="shipping">
-            <h3>Shipping Address</h3>
-            <div className="row1">
-              <div className="first-name">
-                <label htmlFor="">First Name</label>
-                <input type="text" onChange={(e) => {
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <div>
+      <div className="container">
+        <div className="form">
+          <form>
+            <div className="title">
+              <h2>Shipping &amp; Billing</h2>
+            </div>
+            <div className="shipping">
+              <h3>Shipping Address</h3>
+              <div className="row1">
+                <div className="first-name">
+                  <label htmlFor="">First Name</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
                       setFirstName(e.target.value);
-                    }} value={firstname}/>
-              </div>
-              <div className="last-name">
-                <label htmlFor="">Last Name</label>
-                <input type="text" onChange={(e) => {
+                    }}
+                    value={firstname}
+                  />
+                </div>
+                <div className="last-name">
+                  <label htmlFor="">Last Name</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
                       setLastName(e.target.value);
-                    }} value={lastname}/>
+                    }}
+                    value={lastname}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="row2">
-              <div className="address">
-                <label htmlFor="">Address</label>
-                <input type="text" onChange={(e) => {
+              <div className="row2">
+                <div className="address">
+                  <label htmlFor="">Address</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
                       setAddress(e.target.value);
-                    }} value={address}/>
-              </div>
-              <div className="postal-code">
-                <label htmlFor="">ZIP/Postal Code</label>
-                <input type="text"  onChange={(e) => {
+                    }}
+                    value={address}
+                  />
+                </div>
+                <div className="postal-code">
+                  <label htmlFor="">ZIP/Postal Code</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
                       setZip(e.target.value);
-                    }} value={zip} />
+                    }}
+                    value={zip}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="row3">
-              <div className="city">
-                <label htmlFor="">City</label>
-                <input type="text"   onChange={(e) => {
+              <div className="row3">
+                <div className="city">
+                  <label htmlFor="">City</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
                       setCity(e.target.value);
-                    }} value={city} />
-              </div>
-              <div className="country">
-                <div className="label">
-                  <label htmlFor="">Country</label>
+                    }}
+                    value={city}
+                  />
                 </div>
-                <div className="input">
-                  <input type="text" onChange={(e) => {
-                      setCountry(e.target.value);
-                    }} value={country} />
+                <div className="country">
+                  <div className="label">
+                    <label htmlFor="">Country</label>
+                  </div>
+                  <div className="input">
+                    <input
+                      type="text"
+                      onChange={(e) => {
+                        setCountry(e.target.value);
+                      }}
+                      value={country}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <br />
-          <br />
-          <div className="billing">
-            <h3>Payment Details</h3>
-            <div className="row1"></div>
+            <br />
+            <br />
+            <div className="billing">
+              <h3>Payment Details</h3>
+              <div className="row1"></div>
 
-            <div className="row3">
-              <div className="address">
-                <label htmlFor="">Card Holder</label>
-                <input type="text" onChange={(e) => {
+              <div className="row3">
+                <div className="address">
+                  <label htmlFor="">Card Holder</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
                       setCardholderName(e.target.value);
-                    }}value={cardholdername} />
-              </div>
-              <div className="postal-code">
-                <label htmlFor="">Card Number</label>
-                <input type="text"  onChange={(e) => {
-                      setCard(e.target.value);
-                    }} value={card}/>
-              </div>
-            </div>
-            <div className="row4">
-              <div className="city">
-                <label htmlFor="">CVV</label>
-                <input type="text" />
-              </div>
-              <div className="country">
-                <div className="label">
-                  <label htmlFor="">Expiration Date (MM/YY)</label>
+                    }}
+                    value={cardholdername}
+                  />
                 </div>
-                <div className="input">
+                <div className="postal-code">
+                  <label htmlFor="">Card Number</label>
+                  <input
+                    type="text"
+                    onChange={(e) => {
+                      setCard(e.target.value);
+                    }}
+                    value={card}
+                  />
+                </div>
+              </div>
+              <div className="row4">
+                <div className="city">
+                  <label htmlFor="">CVV</label>
                   <input type="text" />
                 </div>
+                <div className="country">
+                  <div className="label">
+                    <label htmlFor="">Expiration Date (MM/YY)</label>
+                  </div>
+                  <div className="input">
+                    <input type="text" />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="row5">
-            <div className="button">
-              <button onClick={handleSubmit}>Proceed to Payment </button>
+            <div className="row5">
+              <div className="button">
+                <button onClick={handleSubmit}>Proceed to Payment </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-);
- 
-                  }
-
-
-  
-
-
-
-
+  );
+}
