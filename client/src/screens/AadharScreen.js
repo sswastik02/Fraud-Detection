8e4b044ecaac0583e6b85391617652603828c1f4
docilefiasco <epshita.hs@gmail.com>
2022-07-30 19:02:47 +0530

    aadhar cloudinary react connection made

--- client/src/screens/AadharScreen.js
+++ client/src/screens/AadharScreen.js
@@ -7,84 +7,82 @@ import { useState } from "react";
 
 function Aadhar() {
   const uid = localStorage.getItem("USER_ID");
-  console.log(uid);
-  const [firstname, setFirstName] = useState("");
-  const [lastname, setLastName] = useState("");
-  const [age, setAge] = useState("");
+  const [fileInputState, setFileInputState] = useState('');
+  const [previewSource, setPreviewSource] = useState('');
+  const [selectedFile, setSelectedFile] = useState();
+  const [successMsg, setSuccessMsg] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const handleFileInputChange = (e) => {
+      const file = e.target.files[0];
+      previewFile(file);
+      setSelectedFile(file);
+      setFileInputState(e.target.value);
+  };
 
- 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const previewFile = (file) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+          setPreviewSource(reader.result);
+      };
+  };
 
-   
+  const handleSubmitFile = (e) => {
+      e.preventDefault();
+      if (!selectedFile) return;
+      const reader = new FileReader();
+      reader.readAsDataURL(selectedFile);
+      reader.onloadend = () => {
+          uploadImage(reader.result);
+      };
+      reader.onerror = () => {
+          console.error('AHHHHHHHH!!');
+          setErrMsg('something went wrong!');
+      };
+  };
 
-    await axios
-      .put(`http://localhost:5000/api/userinfo/${uid}`, {
-        firstname: firstname,
-        lastname: lastname,
-        age: age,
-       
-      })
-      .then((res) => {
-        console.log(res);
-        
-      })
-
-      .catch((err) => console.log(err));
+  const uploadImage = async (base64EncodedImage) => {
+      try {
+          await fetch('/api/upload', {
+              method: 'POST',
+              body: JSON.stringify({ data: base64EncodedImage }),
+              headers: { 'Content-Type': 'application/json' },
+          });
+          setFileInputState('');
+          setPreviewSource('');
+          setSuccessMsg('Image uploaded successfully');
+      } catch (err) {
+          console.error(err);
+          setErrMsg('Something went wrong!');
+      }
   };
   return (
+   
+
     <div>
-      <div className="container">
-        <div className="form">
-          <form>
-            <div className="title">
-              <h2>Student Discount</h2>
-            </div>
-            <div className="shipping">
-              <h3>Upload Aadhar Card to Verify your Age</h3>
-              <div className="row1">
-                <div className="first-name">
-                  <label htmlFor="">First Name</label>
-                  <input type="text" onChange={(e) => {
-                      setFirstName(e.target.value);
-                    }}
-                    value={firstname}/>
-                </div>
-                <div className="last-name">
-                  <label htmlFor="">Last Name</label>
-                  <input type="text"   onChange={(e) => {
-                      setLastName(e.target.value);
-                    }}/>
-                </div>
-              </div>
-
-              <div className="row3">
-                <div className="info">
-                  <div className="label">
-                    <label for="file">
-                      Upload a Aadhar Pdf:{" "}
-                      <input type="file" name="file" />
-                    </label>
-                    <label for="age">
-                      Input you age (years):
-                      <input type="text"   onChange={(e) => {
-                      setAge(e.target.value);
-                    }} />
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="row5">
-              <div className="button">
-                <button  onClick={handleSubmit}>Proceed to Verify </button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+    <h1 className="title">Upload an Image</h1>
+    
+    <form onSubmit={handleSubmitFile} className="form">
+        <input
+            id="fileInput"
+            type="file"
+            name="image"
+            onChange={handleFileInputChange}
+            value={fileInputState}
+            className="form-input"
+        />
+        <button className="btn" type="submit">
+            Submit
+        </button>
+    </form>
+    {previewSource && (
+        <img
+            src={previewSource}
+            alt="chosen"
+            style={{ height: '300px' }}
+        />
+    )}
+</div>
   );
 }
 
